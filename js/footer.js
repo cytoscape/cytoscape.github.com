eba020d03ee2581cf78ce70d076e1a4620ffd53e
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2010-09-10 16:45:00 +0000

    Updated based on Trey's requests.

--- js/footer.js
+++ js/footer.js
@@ -1,2 +1,6 @@
 // Footer
-document.write('<div id="footer">&copy;2001-2010 Cytoscape Consotrium</div>');
+document.write('<div id="footer"><p>Funding for Cytoscape is provided by a federal grant from the U.S. ' +
+	'<a href="http://www.nigms.nih.gov">National Institute of General Medical Sciences (NIGMS)</a> ' +
+	'of the <a href="http://www.nih.gov">National Institutes of Health (NIH)</a> under award ' +
+	'number GM070743-01 and the U.S. <a href="http://www.nsf.gov">National Science Foundation ' +
+	'(NSF) </a>.</p><p>&copy;2001-2010 Cytoscape Consotrium</p></div>');
