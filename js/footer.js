2ccdfa8f69554485a7fee24572a866cb105f6b19
apico <apico@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-03-05 20:59:14 +0000

    added new P41 grant number to footer

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,7 @@ document.write(
 	'<div id="footer"><p>' +
 	'Funding for Cytoscape is provided by a federal grant from <a href="http://www.nigms.nih.gov/About/Overview/bbcb.htm">' +
 	'NIGMS Division of Biomedical Technology, Bioinformatics, and Computational Biology (BBCB)</a> ' +
-	', <a href="http://nrnb.org">National Resource for Network Biology (NRNB)</a> RR031228,' +
+	', <a href="http://nrnb.org">National Resource for Network Biology (NRNB)</a> under award numbers P41 RR031228 and GM103504,' +
 	' and the U.S. <a href="http://www.nigms.nih.gov">National Institute of General Medical Sciences (NIGMS)</a> ' +
 	'of the <a href="http://www.nih.gov">National Institutes of Health (NIH)</a> under award ' +
 	'number GM070743.</p>' +
