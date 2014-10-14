40504a816afc2d5a3de1a801ca205ad549fe40a0
Keiichiro Ono <keiono@gmail.com>
2014-10-14 15:36:53 -0700

    New download page has been added.

--- js/setup_page.js
+++ js/setup_page.js
@@ -88,8 +88,14 @@ $(document).ready(function () {
         'type=\"text\" name=\"q\">' +
         '<button class="btn btn-default" type=\"submit\" name=\"btnG\">Search</button>' +
         '</form></div></div>' +
-        '<script src="http://www.google-analytics.com/urchin.js"></script>' +
-        '<script type="text/javascript">_uacct = "UA-155159-2"; urchinTracker();</script></div>';
+        '<script>' +
+        '(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){' +
+        '    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),' +
+        '    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)' +
+        '})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');' +
+        'ga(\'create\', \'UA-155159-2\', \'auto\');' +
+        'ga(\'send\', \'pageview\');'+
+        '</script></div>';
 
     $('footer').append(footerText);
 
