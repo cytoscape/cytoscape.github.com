919bfe36c3a823bc372a3db2aa52a30447ea14ff
Keiichiro Ono <keiono@gmail.com>
2018-09-26 18:21:24 -0700

    New bug report page added.

---
+++ js/bug-report.js
@@ -0,0 +1,1 @@
+https: //cytoscape.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e-T/b55nvt/b/2/a44af77267a987a660377e5c46e0fb64/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-UK&collectorId=c426866d
--- js/setup_page.js
+++ js/setup_page.js
@@ -5,7 +5,7 @@
  * Time: 10:32
  * To change this template use File | Settings | File Templates.
  */
-$(document).ready(function () {
+$(document).ready(function() {
 
     ////////////////////////////////////
     // Modify this when releasing new version!
@@ -32,76 +32,73 @@ $(document).ready(function () {
 
     var responsiveNav =
         '<nav class="navbar navbar-default" role="navigation">' +
-            '<div class="container">' +
+        '<div class="container">' +
 
-            '<div class="navbar-header">' +
-            '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#cy-navbar">' +
-            '<span class="sr-only">Toggle navigation</span>' +
-            '<span class="icon-bar"></span>' +
-            '<span class="icon-bar"></span>' +
-            '<span class="icon-bar"></span>' +
-            '</button>' +
-            '<a class="navbar-brand" href="index.html">' +
-                '<img src="images/logo/cy3logoOrange.svg" style="height: 43px !important; margin-top: -13px; margin-left: -1em;"/>' +
-            '</a>' +
-            '</div>' +
+        '<div class="navbar-header">' +
+        '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#cy-navbar">' +
+        '<span class="sr-only">Toggle navigation</span>' +
+        '<span class="icon-bar"></span>' +
+        '<span class="icon-bar"></span>' +
+        '<span class="icon-bar"></span>' +
+        '</button>' +
+        '<a class="navbar-brand" href="index.html">' +
+        '<img src="images/logo/cy3logoOrange.svg" style="height: 43px !important; margin-top: -13px; margin-left: -1em;"/>' +
+        '</a>' +
+        '</div>' +
 
-            '<div class="collapse navbar-collapse" id="cy-navbar">' +
-            '<ul class="nav navbar-nav">' +
-            '<li class="dropdown">' +
-            '<a href="#" class="dropdown-toggle" data-toggle="dropdown">Intro<b class="caret"></b></a>' +
-            '<ul class="dropdown-menu">' +
-            '<li><a href="what_is_cytoscape.html">What is Cytoscape?</a></li>' +
-            '<li><a href="who_is_using.html">Who is Using Cytoscape</a></li>' +
-            '<li><a href="stat.html">Project Statistics</a></li>' +
-            '<li><a href="screenshots.html">Screenshots</a></li>' +
-            '<li><a href="roadmap.html">Roadmap</a></li>' +
-            '</ul></li>' +
+        '<div class="collapse navbar-collapse" id="cy-navbar">' +
+        '<ul class="nav navbar-nav">' +
+        '<li class="dropdown">' +
+        '<a href="#" class="dropdown-toggle" data-toggle="dropdown">Intro<b class="caret"></b></a>' +
+        '<ul class="dropdown-menu">' +
+        '<li><a href="what_is_cytoscape.html">What is Cytoscape?</a></li>' +
+        '<li><a href="who_is_using.html">Who is Using Cytoscape</a></li>' +
+        '<li><a href="stat.html">Project Statistics</a></li>' +
+        '<li><a href="screenshots.html">Screenshots</a></li>' +
+        '<li><a href="roadmap.html">Roadmap</a></li>' +
+        '</ul></li>' +
 
-            '<li><a href="download.php">Download</a></li>' +
+        '<li><a href="download.php">Download</a></li>' +
 
-            '<li><a href="http://apps.cytoscape.org/" target="_blank">Apps</a></li>' +
+        '<li><a href="http://apps.cytoscape.org/" target="_blank">Apps</a></li>' +
 
-            '<li id="docs" class="dropdown">' +
-            '<a href="#" class="dropdown-toggle" data-toggle="dropdown">Documentation<b class="caret"></b></a>' +
-            '<ul class="dropdown-menu">' +
-            '<li><a href="releasenotes.html">Release Notes</a></li>' +
-            '<li><a href="documentation_users.html">For Users</a></li>' +
-            '<li><a href="documentation_developers.html">For Developers</a></li>' +
-            '<li><a href="http://wiki.cytoscape.org/">Wiki</a></li>' +
-            '<li><a href="http://opentutorials.cgl.ucsf.edu/index.php/Portal:Cytoscape3">Open Tutorials</a></li>' +
-            '</ul></li>' +
+        '<li id="docs" class="dropdown">' +
+        '<a href="#" class="dropdown-toggle" data-toggle="dropdown">Documentation<b class="caret"></b></a>' +
+        '<ul class="dropdown-menu">' +
+        '<li><a href="releasenotes.html">Release Notes</a></li>' +
+        '<li><a href="documentation_users.html">For Users</a></li>' +
+        '<li><a href="documentation_developers.html">For Developers</a></li>' +
+        '<li><a href="http://wiki.cytoscape.org/">Wiki</a></li>' +
+        '<li><a href="http://opentutorials.cgl.ucsf.edu/index.php/Portal:Cytoscape3">Open Tutorials</a></li>' +
+        '</ul></li>' +
 
-            '<li class="dropdown">' +
-            '<a href="#" class="dropdown-toggle" data-toggle="dropdown">Community<b class="caret"></b></a>' +
-            '<ul class="dropdown-menu">' +
-            '<li class="warn"><a href="http://nrnb.org/competitions.html">Competitions</a></li>' +
-            '<li><a href="development_team.html">Developer Team</a></li>' +
-            '<li><a href="community.html">Social Media</a></li>' +
-            '<li><a href="http://nrnb.org/media-symposia.html">Conferences</a></li>' +
-            '<li><a href="http://nrnb.org/training.html">Training</a></li>' +
-            '</ul></li>' +
+        '<li class="dropdown">' +
+        '<a href="#" class="dropdown-toggle" data-toggle="dropdown">Community<b class="caret"></b></a>' +
+        '<ul class="dropdown-menu">' +
+        '<li class="warn"><a href="http://nrnb.org/competitions.html">Competitions</a></li>' +
+        '<li><a href="development_team.html">Developer Team</a></li>' +
+        '<li><a href="community.html">Social Media</a></li>' +
+        '<li><a href="http://nrnb.org/media-symposia.html">Conferences</a></li>' +
+        '<li><a href="http://nrnb.org/training.html">Training</a></li>' +
+        '</ul></li>' +
 
-            '<li><a href="http://chianti.ucsd.edu/cyto_web/bugreport/bugreport.php">Report a Bug</a></li>' +
+        '<li><a href="http://chianti.ucsd.edu/cyto_web/bugreport/bugreport.php">Report a Bug</a></li>' +
 
-            '<li class="dropdown">' +
-            '<a href="#" class="dropdown-toggle cy3-color" data-toggle="dropdown">Help<b class="caret"></b></a>' +
-            '<ul class="dropdown-menu">' +
-            '<li><a href="troubleshooting.html">Troubleshooting</a></li>' +
-            '<li><a href="https://groups.google.com/forum/#!forum/cytoscape-helpdesk">Getting Help</a></li>' +
-            '</ul></li>' +
+        '<li class="dropdown">' +
+        '<a href="#" class="dropdown-toggle cy3-color" data-toggle="dropdown">Help<b class="caret"></b></a>' +
+        '<ul class="dropdown-menu">' +
+        '<li><a href="troubleshooting.html">Troubleshooting</a></li>' +
+        '<li><a href="https://groups.google.com/forum/#!forum/cytoscape-helpdesk">Getting Help</a></li>' +
+        '</ul></li>' +
 
-            '<li><gcse:searchbox-only newWindow="true"></gcse:searchbox-only></li>' +
+        '<li><gcse:searchbox-only newWindow="true"></gcse:searchbox-only></li>' +
 
-            '</ul>' +
+        '</ul>' +
 
-            '</div></div></nav>';
+        '</div></div></nav>';
 
     navContainer.append(responsiveNav);
     $('body').prepend(navContainer);
-
-    // Job Link
-    $('<li class="dropdown"><a href="jobs.html" style="color: red; font-weight: 700;">Jobs</a></li>').insertAfter('#docs');
 
     // Footer
     var footerText = '<div class="container"><div class="row">' +
@@ -124,16 +121,16 @@ $(document).ready(function () {
         '    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)' +
         '})(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\');' +
         'ga(\'create\', \'UA-155159-2\', \'auto\');' +
-        'ga(\'send\', \'pageview\');'+
+        'ga(\'send\', \'pageview\');' +
         '</script></div>';
 
-        '</div></div></div>';
+    '</div></div></div>';
 
     var googleAnalytics = '<script type="text/javascript">' +
-                          "var _gaq = _gaq|| []; _gaq.push(['_setAccount', 'UA-155159-2']);" +
-                           "_gaq.push(['_trackPageview']); (function() { var ga = document.createElement('script');" +
-                            "ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';"+
-                            "var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })(); </script>";
+        "var _gaq = _gaq|| []; _gaq.push(['_setAccount', 'UA-155159-2']);" +
+        "_gaq.push(['_trackPageview']); (function() { var ga = document.createElement('script');" +
+        "ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';" +
+        "var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })(); </script>";
 
     $('footer').append(footerText);
     $('body').append(googleAnalytics);
