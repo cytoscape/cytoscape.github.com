627859caf480f19cf5ca30a425fb02715a7c3bc0
Alexander Pico <apico@gladstone.ucsf.edu>
2022-05-22 16:05:35 -0700

    Update js4cytoscape-dev.js

--- js/js4cytoscape-dev.js
+++ js/js4cytoscape-dev.js
@@ -1,3 +1,5 @@
+const defaultBaseUrl = 'http://127.0.0.1:1234/v1';
+
 let cyrunning;
 let cybadgeKey;
 let cybadgeVal;
@@ -9,14 +11,15 @@ async function cyBadge() {
   try {
     cyversion = await cyrestGET('version');
     cyrunning = true;
+    console.log('cytoscape v' + cyversion["cytoscapeVersion"] + ' is running');
   } catch(err){
     cyrunning = false;
+    console.log('cytoscape is not running');
   }
 
   cybadgeElm = document.getElementsByClassName('cytoscape-badge');
   //just in case there is more than on badge on a page
   for(let i = 0; i < cybadgeElm.length; i++) {
-    console.log(cybadgeElm[i].id + " ");
     cybadgeKey = document.createElement('span');
     cybadgeKey.className = 'cybadgekey';
     cybadgeKey.innerHTML = 'cytoscape';
@@ -32,4 +35,21 @@ async function cyBadge() {
     cybadgeElm[i].appendChild(cybadgeKey);
     cybadgeElm[i].appendChild(cybadgeVal);
   } 
+}
+
+async function cyrestGET(operation = '', parameters = '', baseUrl = defaultBaseUrl) {
+    let qurl = baseUrl.concat('/', operation);
+    if (parameters != '') {
+        const qparameters = parameters;
+        qurl = qurl.concat('?', qparameters);
+    }
+    const res = await fetch(qurl, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        }
+      });
+      const json = await res.json();
+      return json;
 }
