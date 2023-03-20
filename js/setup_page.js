8d29fc2d5332a2dd9b1900c6396ed7db9c067e96
Alexander Pico <apico@gladstone.ucsf.edu>
2023-03-20 16:19:51 -0700

    fixed quotes

--- js/setup_page.js
+++ js/setup_page.js
@@ -140,11 +140,11 @@ $(document).ready(function() {
         '(function(h,o,t,j,a,r){' +
         'h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};' +
         'h._hjSettings={hjid:3415751,hjsv:6};' +
-        'a=o.getElementsByTagName('head')[0];' +
-        'r=o.createElement('script');r.async=1;' +
+        "a=o.getElementsByTagName('head')[0];" +
+        "r=o.createElement('script');r.async=1;" +
         'r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;' +
         'a.appendChild(r);' +
-        '})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');' +
+        "})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');" +
         '</script>'
 
     $('footer').append(footerText);
